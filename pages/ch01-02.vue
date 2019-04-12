c274b785146e80e12a924f20df146c5b09d68899
SENGA <jeuxleaveus@gmail.com>
2019-04-12 20:37:59 +0900

    Completed up to Chapter-01 Materials light animation

---
+++ pages/ch01-02.vue
@@ -0,0 +1,83 @@
+<template>
+  <div ref="stage"></div>
+</template>
+
+<script>
+import * as THREE from 'three'
+
+export default {
+  name: 'FirstScene',
+  data() {
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setClearColor(new THREE.Color(0xeeeeee))
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    const axes = new THREE.AxesHelper(20)
+    scene.add(axes)
+
+    const planeGeometry = new THREE.PlaneGeometry(60, 20)
+    const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc })
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+
+    plane.rotation.x = -0.5 * Math.PI
+    plane.position.x = 15
+    plane.position.y = 0
+    plane.position.z = 0
+
+    scene.add(plane)
+
+    const cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
+    const cubeMaterial = new THREE.MeshBasicMaterial({
+      color: 0xff0000,
+      wireframe: true
+    })
+    // const cubeMaterial = new THREE.LineBasicMaterial({
+    //   color: 0xff0000,
+    //   linewidth: 1
+    // })
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+
+    cube.position.x = -4
+    cube.position.y = 3
+    cube.position.z = 0
+
+    scene.add(cube)
+
+    const sphereGeometry = new THREE.SphereGeometry(4, 20, 20)
+    const sphereMaterial = new THREE.MeshBasicMaterial({
+      color: 0x7777ff,
+      wireframe: true
+    })
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+
+    sphere.position.x = 20
+    sphere.position.y = 4
+    sphere.position.z = 2
+
+    scene.add(sphere)
+
+    camera.position.x = -30
+    camera.position.y = 40
+    camera.position.z = 30
+    camera.lookAt(scene.position)
+
+    return {
+      scene,
+      renderer,
+      camera
+    }
+  },
+  mounted() {
+    this.$refs.stage.appendChild(this.renderer.domElement)
+    this.renderer.render(this.scene, this.camera)
+  }
+}
+</script>
---
+++ pages/ch01-04.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <div ref="statsOutput"></div>
+    <div ref="stage"></div>
+  </div>
+</template>
+
+<script>
+import * as THREE from 'three'
+import Stats from 'stats.js'
+
+export default {
+  name: 'MaterialLightAnimation',
+  data() {
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setClearColor(new THREE.Color(0xeeeeee))
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.shadowMap.enabled = true
+
+    const axes = new THREE.AxesHelper(20)
+    scene.add(axes)
+
+    const planeGeometry = new THREE.PlaneGeometry(60, 20)
+    const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff })
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+
+    plane.rotation.x = -0.5 * Math.PI
+    plane.position.x = 15
+    plane.position.y = 0
+    plane.position.z = 0
+    plane.receiveShadow = true
+
+    scene.add(plane)
+
+    const cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
+    const cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 })
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+
+    cube.position.x = -4
+    cube.position.y = 3
+    cube.position.z = 0
+    cube.castShadow = true
+
+    scene.add(cube)
+
+    const sphereGeometry = new THREE.SphereGeometry(4, 20, 20)
+    const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff })
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+
+    sphere.position.x = 20
+    sphere.position.y = 4
+    sphere.position.z = 2
+    sphere.castShadow = true
+
+    scene.add(sphere)
+
+    camera.position.x = -30
+    camera.position.y = 40
+    camera.position.z = 30
+    camera.lookAt(scene.position)
+
+    const spotLight = new THREE.SpotLight(0xffffff)
+    spotLight.position.set(-20, 30, -5)
+    spotLight.castShadow = true
+    scene.add(spotLight)
+
+    const stats = new Stats()
+    stats.setMode(0)
+    stats.domElement.style.position = 'absolute'
+    stats.domElement.style.left = '0px'
+    stats.domElement.style.top = '0px'
+
+    return {
+      scene,
+      renderer,
+      camera,
+      stats
+    }
+  },
+  mounted() {
+    this.$refs.statsOutput.appendChild(this.stats.domElement)
+    this.$refs.stage.appendChild(this.renderer.domElement)
+    this.renderScene()
+  },
+  methods: {
+    renderScene() {
+      this.stats.update()
+      // requestAnimationFrame(this.renderScene)
+      this.renderer.render(this.scene, this.camera)
+    }
+  }
+}
+</script>
